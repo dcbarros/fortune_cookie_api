029594296a2ddd00a9f357ac6927f8408fd352d5
dcbarros <dcbarros1@gmail.com>
2024-09-19 14:21:43 -0300

    feat: adiciona adiministrador que terá acesso a plataforma

---
+++ src/main/java/com/biscoitosorte/biscoito_sorte/model/User.java
@@ -0,0 +1,15 @@
+// package com.biscoitosorte.biscoito_sorte.model;
+
+// import jakarta.persistence.Entity;
+// import lombok.Getter;
+// import lombok.Setter;
+
+// // @Entity
+// @Getter
+// @Setter
+// public class User {
+    
+//     private Long id;
+//     private String email;
+//     private String password;
+// }
