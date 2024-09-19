6f747e417039c46f7b1ebec7ad384dccfdcc4890
dcbarros <dcbarros1@gmail.com>
2024-09-19 14:16:03 -0300

    feat: Inicialização do repositório

---
+++ src/main/java/com/biscoitosorte/biscoito_sorte/BiscoitoSorteApplication.java
@@ -0,0 +1,13 @@
+package com.biscoitosorte.biscoito_sorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiscoitoSorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiscoitoSorteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biscoitosorte/biscoito_sorte/BiscoitoSorteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biscoitosorte.biscoito_sorte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiscoitoSorteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
